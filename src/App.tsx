9ec7fe72b1ebb901dd9d079d4f6057a2bdca9ea8
DavJesse <jesseomolo@gmail.com>
2025-11-23 20:28:56 +0300

    chore: implement router (#19)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import Navbar from './components/Navbar';
-import { BlogSection } from './components/BlogSection';
+import React, { useState, useEffect, useCallback } from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { BlogSection } from "./components/BlogSection";
 import { Hero } from "./components/Hero";
-import { ProjectsSection } from './components/ProjectsSection';
-import ContactSection from './components/ContactSection'
-import { Footer } from './components/Footer';
+import { ProjectsSection } from "./components/ProjectsSection";
+import ContactSection from "./components/ContactSection";
+import { Footer } from "./components/Footer";
+import OurStory from "./pages/OurStory";
 
 export default function App() {
   const SCROLL_THRESHOLD = 75;
@@ -28,10 +30,23 @@ export default function App() {
   return (
     <div className="min-h-screen w-full bg-[#050505] text-white overflow-x-hidden selection:bg-[#C7F246] selection:text-black">
       <Navbar scrolled={isScrolled} />
-      <Hero />
-      <ProjectsSection />
-      <BlogSection />
-      <ContactSection />
+      <Routes>
+        {/* Home Page */}
+        <Route
+          path="/"
+          element={
+            <>
+              <Hero />
+              <ProjectsSection />
+              <BlogSection />
+              <ContactSection />
+            </>
+          }
+        />
+
+        {/* Our Story Page */}
+        <Route path="/our-story" element={<OurStory />} />
+      </Routes>
       <Footer />
     </div>
   );
