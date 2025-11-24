5781b849ccb64276d088e05a38422d4d520d8428
nyunja <poptiff@gmail.com>
2025-11-24 11:31:27 +0300

    Add AllPostsPage route and update BlogSection with Link navigation
    - Add /posts route to router.tsx for AllPostsPage
    - Update BlogSection to use Link component for 'View all posts' navigation
    - Remove onViewAll callback prop in favor of Link-based routing
    - Enable proper URL updates and browser history for blog navigation

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from './App';
 import { AllProjectsPage } from './components/AllProjectsPage';
+import { AllPostsPage } from './components/AllPostsPage';
 import { Hero } from './components/Hero';
 import { ProjectsSection } from './components/ProjectsSection';
 import { BlogSection } from './components/BlogSection';
@@ -26,6 +27,10 @@ const router = createBrowserRouter([
         path: '/projects',
         element: <AllProjectsPage />,
       },
+      {
+        path: '/blog',
+        element: <AllPostsPage onBack={() => window.history.back()} />,
+      }
     ],
   },
 ]);
